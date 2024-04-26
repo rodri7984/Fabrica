0742aafd89fab85bbe154e9442cdd96454a2a636
rodri7984 <127908368+rodri7984@users.noreply.github.com>
2024-04-26 19:47:45 -0400

    tabla consulta a la api boton filtro no funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {EmpAddEditComponent} from './emp-add-edit/emp-add-edit.component';
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
 import { Router } from '@angular/router';
+import {MatTableModule, MatTableDataSource} from '@angular/material/table';
+import { ClienteService } from './core/services/cliente.service';
+import { HttpClient } from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 @Component({
@@ -18,16 +23,41 @@ import { Router } from '@angular/router';
             MatButtonModule,
             EmpAddEditComponent,
             MatDialogModule,
-            MatFormFieldModule
+            MatFormFieldModule,
+            MatTableModule,
+            MatInputModule
+            
+            
+            
+            
           ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'fabricaApp';
-  constructor(private _dialog: MatDialog , private router : Router) {}
+  public desplegarColumna : string[] = ['run','dv','primerNombre','paternoApellido','tipoUsuario'];
+  dataSource = new MatTableDataSource();
+  constructor(private _dialog: MatDialog , private router : Router , private http: HttpClient, private clienteService: ClienteService) {}
+
+  ngOnInit(): void {
+    this.mostrarDatos()
+  }
+
 
   openAddEditEmpForm() {
     this._dialog.open(EmpAddEditComponent);
+  }
+
+
+  mostrarDatos(){
+    this.clienteService.getMetodo();
+    this.dataSource =this.clienteService.getJsonValue;
+
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
--- src/app/core/services/cliente.service.ts
+++ src/app/core/services/cliente.service.ts
@@ -10,11 +10,23 @@ export class ClienteService {
   private apiUrl = 'http://localhost:8080/usuarios';
   
 
-  constructor(private httpClient: HttpClient) {}
+
+  constructor(private httpClient: HttpClient) { }
 
   agregarUsuario(usuario: any) {
     return this.httpClient.post(`${this.apiUrl}/createUsuario`, usuario);
   }
-}
 
+  public getJsonValue: any;
+
+  public getMetodo() {
+    return this.httpClient.get(`${this.apiUrl}/getUsuarios`).subscribe((data) => {
+      console.table(data);
+      this.getJsonValue = data;
+      
+    });
+
+
+  }
+}
 
