e02b0918a041a4375913aaa95333036d7eef50cb
Fabián Alexis Calderón Pérez <sabo0p731@gmail.com>
2024-05-31 23:30:26 -0400

    Cambio estado funcional
    viva viva viva, ahora me voy a jugar

--- src/app/componentes/mostrarStaff/mostrar-staff/mostrar-staff.component.ts
+++ src/app/componentes/mostrarStaff/mostrar-staff/mostrar-staff.component.ts
@@ -8,14 +8,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
-import { MatInputModule } from '@angular/material/input';import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input'; import { CommonModule } from '@angular/common';
 import dayjs from 'dayjs';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { Colaborador } from '../../../modelos/colaborador';
 import { ColaboradorService } from '../../../core/services/colaborador.service';
 import { ColaboradorComponent } from '../../form-colaboradores/colaboradores-component.component';
-import { RelacionActividadColaboradorComponent } from '../../../relacion-actividad-colaborador/relacion-actividad-colaborador.component';
+// import { RelacionActividadColaboradorComponent } from '../../../relacion-actividad-colaborador/relacion-actividad-colaborador.component';
 import { escape } from 'querystring';
 
 @Component({
@@ -48,7 +48,7 @@ export class MostrarStaffComponent {
     this.sidenav.close();
   }
 
-  desplegarColumna: string[] = ['nombre','username','rolColaborador','estado', 'acciones'];
+  desplegarColumna: string[] = ['nombre', 'username', 'rolColaborador', 'estado', 'acciones'];
   dataSource = new MatTableDataSource<Colaborador>();
   // planes: { [key: string]: number } = {};
 
@@ -57,50 +57,62 @@ export class MostrarStaffComponent {
     private router: Router,
     private http: HttpClient,
     private colaboradorService: ColaboradorService,
-    
+
   ) { }
   ngOnInit(): void {
     this.colaboradorService.obtenercolaboradorDesdeAPI().subscribe((data) => {
       this.usuarios = data;
       this.dataSource.data = this.usuarios; // Asegúrate de actualizar la dataSource aquí
     });
-}
-
-openColaborador() {
-  this._dialog.open(ColaboradorComponent);
-}
-
-applyFilter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
-
-relacionarEstado(staff: Colaborador) {
-  const dialogRef = this._dialog.open(RelacionActividadColaboradorComponent, {
-    width: '500px',
-    data: { staff }
-  });
-}
-
-cambiarEstado(staff: Colaborador){
-  console.log(staff);
-  console.log('pasa por aqui');
-  if(staff.estado === "ACTIVO"){
-    console.log('no pasa');
-    return this.colaboradorService.cambiarEstadoColaborador(staff.username, "INACTIVO");
-    console.log('pasó');
-  } else {
-    return this.colaboradorService.cambiarEstadoColaborador(staff.username, "ACTIVO");
-
   }
-}
 
-changeEstado(staff: Colaborador){
-  console.log('...');
-  console.log(staff);
-  return this.colaboradorService.cambiarEstadoColaborador(staff.username, "INACTIVO");
-  console.log('pasa por aqui porfin');
-  return console.log(staff);
-}
+  openColaborador() {
+    this._dialog.open(ColaboradorComponent);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+
+
+  // cambiarEstado(staff: Colaborador) {
+  //   console.log(staff);
+  //   console.log('...');
+  //   if (staff.estado === "ACTIVO") {
+  //     return this.colaboradorService.cambiarEstadoColaborador(staff.username, "INACTIVO");
+  //   } else {
+  //     return this.colaboradorService.cambiarEstadoColaborador(staff.username, "ACTIVO");
+
+  //   }
+  // }
+
+  // changeEstado(staff: Colaborador) {
+  //   console.log('...');
+  //   console.log(staff);
+  //   return this.colaboradorService.cambiarEstadoColaborador(staff.username, "INACTIVO");
+  //   console.log('pasa por aqui porfin');
+  //   return console.log(staff);
+  // }
+
+  changeEstado(staff: Colaborador) {
+    if (staff.estado === "ACTIVO") {
+      return this.colaboradorService.changeEstadoColab(staff.username, "INACTIVO")
+        .subscribe(response => {
+          console.log('Estado del colaborador actualizado:', response);
+        }, error => {
+          console.error('Error al actualizar el estado del colaborador:', error);
+        });
+        
+    } else {
+      return this.colaboradorService.changeEstadoColab(staff.username, "ACTIVO")
+        .subscribe(response => {
+          console.log('Estado del colaborador actualizado:', response);
+        }, error => {
+          console.error('Error al actualizar el estado del colaborador:', error);
+        });
+    }
+  }
 
 }
--- src/app/core/services/colaborador.service.ts
+++ src/app/core/services/colaborador.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Colaborador } from '../../modelos/colaborador';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColaboradorService {
-  //private apiUrl = 'http://localhost:8080/colaboradores';
+  private apiUrl = 'http://localhost:8080/colaboradores';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -18,10 +19,9 @@ export class ColaboradorService {
     return this.httpClient.get<Colaborador[]>(`${this.apiUrl}/listColaborador`);// Reemplaza 'URL_DE_TU_API' con la URL real de tu API
   }
 
-  private apiUrl = 'http://localhost:8080/colaboradores';
 
   cambiarEstadoColaborador(username: string, estado: string) {
-    const url = `${this.apiUrl}/changeStateColaborador/${username}`;
+    const url = `${this.apiUrl}/changeStateColaborador/${username}/${estado}`;
     this.httpClient.put(url, { estado }).subscribe(
       response => {
         // Manejo de la respuesta exitosa
@@ -34,6 +34,12 @@ export class ColaboradorService {
     );
   }
 
+
+  // Cambiar Estado totalmente funcional
+  changeEstadoColab(username: string, estado: string): Observable<any> {
+    const url = `${this.apiUrl}/changeStateColaborador/${username}/${estado}`;
+    return this.httpClient.put(url, { estado});
+  }
 
   // updateEstado(username: string, estado: string){
   //   return this.httpClient.put<Colaborador[]>(`${this.apiUrl}/changeStateColaborador/${username}/${estado}`);
