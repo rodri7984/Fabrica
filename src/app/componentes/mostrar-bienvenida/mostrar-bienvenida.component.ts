df563a49cd411e0af57fec326fd4392643e387ec
rodri7984 <127908368+rodri7984@users.noreply.github.com>
2024-06-14 14:20:38 -0400

    primer maquetado welcome page

--- src/app/componentes/mostrar-bienvenida/mostrar-bienvenida.component.ts
+++ src/app/componentes/mostrar-bienvenida/mostrar-bienvenida.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
+import { CarouselModule } from 'primeng/carousel';
+
 @Component({
   selector: 'app-mostrar-bienvenida',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule,
+    CarouselModule,
+    
+  ],
   templateUrl: './mostrar-bienvenida.component.html',
   styleUrl: './mostrar-bienvenida.component.css'
 })
-export class MostrarBienvenidaComponent {
+export class MostrarBienvenidaComponent implements OnInit {
+  images!: string[];
 
+  ngOnInit() {
+    this.images = [
+      'assets/img/Screenshot 2024-06-14 131938.png',
+      
+      'assets/img/Screenshot 2024-06-14 133342.png'
+    
+    ];
+  }
 }
